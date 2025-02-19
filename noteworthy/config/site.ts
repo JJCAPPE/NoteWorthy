42b87de3e7039dbef2a246b083a09aa9b6ae7228
JJCAPPE <giacomo.cappelletto@icloud.com>
2025-02-19 12:48:21 -0500

    fixed naming

--- noteworthy/config/site.ts
+++ noteworthy/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "NoteWorthy?",
+  description: "Transcribe your notes and make them lasty forever",
   navItems: [
     {
       label: "Home",
@@ -63,7 +63,5 @@ export const siteConfig = {
     github: "https://github.com/JJCAPPE",
     twitter: "https://twitter.com/hero_ui",
     docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
   },
 };
